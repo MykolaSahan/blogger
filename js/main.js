e4c6363c9151a81e11f886ec8cadffeb43459df7
MykolaSahan <59999729+MykolaSahan@users.noreply.github.com>
2020-03-11 15:39:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  568: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+};
+
+$(document).ready(function() {
+  $nav = $('.nav');
+  $toogleCollapse = $('.toggle-collapse');
+
+  /**  click event on toggle menu */
+  $toogleCollapse.click(function() {
+    $nav.toggleClass('collapse');
+  })
+
+  // own carousel for blog
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
+
+  // click to scroll top
+  $('.move-up span').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1800);
+  });
+
+  // AOS instance
+  AOS.init();
+
+});
